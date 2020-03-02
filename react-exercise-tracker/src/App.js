f364536ecabcb75e2893d924e1aeaf47349392e0
LuizFurtado <luizfpfurtado@hotmail.com>
2020-03-01 23:39:35 -0300

    Frontend - Done

--- react-exercise-tracker/src/App.js
+++ react-exercise-tracker/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/**
+ * Components import: all components used by the react router
+ * These components reflects the routes available at the backend
+ * Each route is created on a separated component.
+ * For code organization and to use custom styled components, you can also
+ * create folders for each component.
+ */
+import Navbar from './components/navbar.component';
+import ExercisesList from './components/exercises-list.component';
+import EditExercise from './components/edit-exercise.component';
+import CreateExercise from './components/create-exercise.component';
+import CreateUser from './components/create-user.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='container'>
+        <Navbar />
+        <br />
+        <Route path='/' exact component={ ExercisesList } />
+        <Route path='/edit/:id' component={ EditExercise } />
+        <Route path='/create' component={ CreateExercise } />
+        <Route path='/user' component={ CreateUser } />
+      </div>
+    </Router>
   );
 }
 
